2d2c2a6e4f466de62badc857c652604829f28626
4xel <aur4lien@gmail.com>
2024-09-25 06:19:01 +0200

    fish xsel

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,14 @@ abbr zj zellij
 abbr d --set-cursor "cd % && ls"
 abbr mkcd --set-cursor 'begin set -l dir % && mkdir -p $dir && cd $dir; end'
 abbr hg "history|grep "
-fish_add_path ~/.cargo/bin/
-set EDITOR "kakoune"
+    # xsel
+abbr xbi "xsel -bi"
+abbr xbo "xsel -bo"
+abbr xpi "xsel -pi"
+abbr xpo "xsel -po"
+abbr xsi "xsel -si"
+abbr xso "xsel -so"
+    set EDITOR "kakoune"
 abbr kds "setsid kak -d -s"
 abbr kc "kak -c"
 abbr 'kc!' --set-cursor 'session_name=% setsid kak -d -s $session_name; kak -c $session_name'
@@ -13,6 +19,7 @@ abbr kl "kak -l"
 alias kakln1c 'kak -c (kak -l | grep -xE "[[:digit:]]+" | head -n 1)'
 alias kake 'setsid /var/lib/snapd/snap/bin/kakoune -d -s EDITOR 2>/dev/null ; /var/lib/snapd/snap/bin/kakoune -c EDITOR'
     fish_add_path ~/.local/bin ~/bin
+fish_add_path ~/.cargo/bin/
 end
 
 
